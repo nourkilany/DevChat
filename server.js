545f553f71d9e499fd3447f41556683105ee10e6
nour <nour.kilany88@gmail.com>
2019-05-12 13:57:10 +0200

    Add database connection

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get('/', (req, res) => res.send("Welcome to API, it's running..."));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
