abd10eac6200fcd5cdcfe4b740fb7902628e4016
nour <nour.kilany88@gmail.com>
2019-05-12 14:25:54 +0200

    Add main routes

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @route   GET api/posts
+ * @desc    Test Route
+ * @access  public
+ */
+router.get('/', (req, res) => res.send('User route'));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send("Welcome to API, it's running..."));
 
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/users', require('./routes/api/users'));
+app.use('/api/v1/profile', require('./routes/api/profile'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT} ...`));
