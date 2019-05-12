f83fcb8367fe682a9d4f62694f0c44cde3a17cb3
nour <nour.kilany88@gmail.com>
2019-05-12 18:31:13 +0200

    Add jwt response to registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
 const User = require('../../models/User');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
 
 /**
  * @route   POST api/users
@@ -57,7 +59,21 @@ router.post(
 
       await user.save();
 
-      res.send('User Registered');
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
     } catch (error) {
       console.log(error.message);
       res.status(500).send('Server error');
